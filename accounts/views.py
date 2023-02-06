be7f56df219829931c2b9d6ecff9365249986315
Tristin <tmille2004@gmail.com>
2023-02-05 23:55:58 -0600

    adds functionality to register view

--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,33 @@ from rest_framework import status
 from django.contrib.auth import authenticate, login
 
 from .serializers import LoginSerializer, RegisterAccountSerializer
+from .models import Account
+
 # Create your views here.
 class Register(APIView):
-    def post(self, request):
-        print('test')
+    serializer_class = RegisterAccountSerializer
 
-        return Response(status=status.HTTP_200_OK)
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = serializer.data.get('email')
+            username = serializer.data.get('username')
+            password = serializer.data.get('password')
+
+            account = Account(email=email, username=username)
+            account.set_password(password)
+            account.save()
+
+            # url = Account.get_activate_url(request, account)
+            # message = render_to_string("template_activate_account.html", {
+            #     "url": url
+            # })
+            # mail_subject = "Activate Your Account"
+            # send_email_task.delay(mail_subject, message, email)
+
+            return Response({"success": "Please confirm your email address"}, status=status.HTTP_201_CREATED)
+
+        return Response({'Bad Request': "Invalid Data..."}, status=status.HTTP_400_BAD_REQUEST)
 
 class Login(APIView):
     serializer_class = LoginSerializer
