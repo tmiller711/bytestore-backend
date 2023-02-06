51d1d0ad1a6efc7b1935c9a9b9852f04fe8c5c22
Tristin <tmille2004@gmail.com>
2023-02-05 22:46:24 -0600

    adds cors headers and allows all origins

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class Register(APIView):
     def post(self, request):
-        pass
+        print('test')
+
+        return Response(status=status.HTTP_200_OK)
 
 class Login(APIView):
     def post(self, request):
-        pass
+        print('test')
+
+        return Response(status=status.HTTP_200_OK)
--- backend/settings.py
+++ backend/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -38,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 
     'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
