0850c339aa281bdaa3e51361338922b3c4f752c2
Tristin <tmille2004@gmail.com>
2023-02-09 02:32:57 -0600

    fixes SIMPLE_JWT spelling error

--- backend/settings.py
+++ backend/settings.py
@@ -116,9 +116,9 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
-    'ROTATE_REFRESH_TOKEN': True,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
